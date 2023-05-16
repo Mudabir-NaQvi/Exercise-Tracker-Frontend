1e859ff9cf625c682f0a6c018e550c55bc32fadf
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2023-05-16 23:49:34 +0500

    Setup application and added register page

---
+++ vite-project/src/Components/Register.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Register.css";
+import registerImage from "../images/register.png";
+export default function Register() {
+  return (
+    <div className="register__container">
+      <div className="register__containerLeft">
+        <img src={registerImage} alt="cycling man" />
+      </div>
+      <div className="register__containerRight">
+        <h2>Register</h2>
+        <form className="register__form">
+            <input type="text" name="firstName" placeholder="First Name"/>
+            <input type="text" name="lastName" placeholder="Last name"/>
+            <input type="email" name="email" placeholder="Email" required/>
+            <input type="password" name="password" placeholder="Password" required/>
+            <input type="submit" value="Submit" />
+        </form>
+      </div>
+    </div>
+  )
+}
