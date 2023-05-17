7f99c277ed40a74bff2d041054dd972b120e5765
Tariq Hussain Chang <tariqhussainchang363@gmail.com>
2023-05-17 18:13:48 +0500

    Added logout feature

--- vite-project/src/Components/Dashboard/Sidebar.jsx
+++ vite-project/src/Components/Dashboard/Sidebar.jsx
@@ -3,28 +3,34 @@ import "./Sidebar.css";
 import HomeIcon from "@mui/icons-material/Home";
 import CreateIcon from "@mui/icons-material/Create";
 import LogoutIcon from "@mui/icons-material/Logout";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
 export default function Sidebar() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+  };
   return (
     <div className="dashboard__sidebar">
       <div className="dashboard__sidebarTop">
         <div className="dashboard__item dashboard__home">
-          <HomeIcon style={{ width: "32", height: "32" }} />
           <Link to="/dashboard" className="link link__home">
-            Home
+            <HomeIcon style={{ width: "32", height: "32" }} />
+            <p>Home</p>
           </Link>
         </div>
         <div className="dashboard__item dashboard__createActivity">
-          <CreateIcon style={{ width: "32", height: "32" }} />
           <Link to="/create-activity" className="link link__create">
-            Create
+            <CreateIcon style={{ width: "32", height: "32" }} />
+            <p>Create</p>
           </Link>
         </div>
       </div>
       <div className="dashboard__item dashboard__logout">
-        <LogoutIcon style={{ width: "32", height: "32" }} />
-        <Link to="/logout" className="link link__create">
-          Logout
+        <Link to="/login" className="link link__create" onClick={handleLogout}>
+          <LogoutIcon style={{ width: "32", height: "32" }} />
+          <p>Logout</p>
         </Link>
       </div>
     </div>
--- vite-project/src/Components/Router/Router.jsx
+++ vite-project/src/Components/Router/Router.jsx
@@ -1,41 +1,24 @@
 import React, { useEffect } from "react";
-import {
-  Routes,
-  Route,
-  useNavigate,
-  useLocation,
-  // useHistory,
-} from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
 import "./Router.css";
 import Dashboard from "../Dashboard/Dashboard";
 import Cookies from "js-cookie";
-import { isLoggedIn } from "./isLoggedIn";
 
 export default function Router() {
   const navigate = useNavigate();
-  // const history = useHistory();
-  // console.log(location);
+  // console.log(location)  ;
+  const token = Cookies.get("token");
   useEffect(() => {
-    const token = Cookies.get("token");
     if (!token) {
       navigate("/login");
-    } else {
-      navigate("../dashboard", { replace: true });
-      // if (location.pathname === "/login") {
-      //   navigate("/dashboard");
-      //   console.log(location.pathname);
-      // }
-      // history.replace("/dashboard");
     }
   }, []);
 
   return (
     <div>
       <Routes>
-        <Route path="/login" element={isLoggedIn("/login", <Login />)} />
-
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
